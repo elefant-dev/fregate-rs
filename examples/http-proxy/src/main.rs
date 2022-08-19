5ec07f1f3708ad7b63fd1162226c0298eef9fe89
nduchenk <adddyny@yandex.ru>
2022-08-19 10:43:23 +0300

    update http-proxy example

--- examples/http-proxy/src/main.rs
+++ examples/http-proxy/src/main.rs
@@ -1,83 +1,61 @@
-use std::future::Future;
-use tokio::net::TcpStream;
-
-use crate::hyper::{upgrade::Upgraded, StatusCode};
-use fregate::axum::{
-    response::{IntoResponse, Response},
-    routing::get,
-    {body, Router},
-};
-use fregate::hyper::{Body, Error, Request};
-use fregate::tower::util::ServiceFn;
-use fregate::tracing::{debug, trace, warn};
-use fregate::{hyper, AppConfig, Application};
-
-type _Svs = ServiceFn<dyn FnOnce(Request<Body>) -> dyn Future<Output = Response>>;
+use fregate::axum::routing::any;
+use fregate::axum::{routing::get, Router};
+use fregate::{http_trace_layer, init_tracing, route_proxy, AppConfig, Application};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(handler));
+    init_tracing();
 
-    // let service = tower::service_fn(move |req: Request<Body>| {
-    //     let router = router.clone();
-    //     async move {
-    //         if req.method() == Method::CONNECT {
-    //             proxy(req).await
-    //         } else {
-    //             router.oneshot(req).await.map_err(|err| match err {})
-    //         }
-    //     }
-    // });
+    std::env::set_var("APP_PROXY_HOST", "127.0.0.1");
+    std::env::set_var("APP_PROXY_PORT", "3000");
 
-    let config = AppConfig::default();
+    // Start server where to proxy requests
+    tokio::spawn(server());
 
-    Application::new(&config)
-        .rest_router(router)
+    // set up your server Routers
+    let hello = Router::new().route("/hello", get(|| async { "Hello" }));
+    let world = Router::new().route("/world", get(|| async { "World" }));
+    let app = Router::new().nest("/app", hello.merge(world));
+
+    // add proxy_router, if no app Router is matched check path in fallback and redirect request
+    let with_proxy = app
+        .fallback(route_proxy("/proxy_server/*path", "http://127.0.0.1:3000"))
+        .layer(http_trace_layer());
+
+    // Or you might want add path to always redirect to another service:
+    // let with_proxy = app
+    //     .merge(route_proxy("/proxy_server/*path", "http://127.0.0.1:3000"))
+    //     .layer(http_trace_layer());
+
+    Application::new(&AppConfig::default())
+        .rest_router(with_proxy)
         .serve()
         .await
         .unwrap();
 }
 
-async fn handler() -> &'static str {
-    "Hello, Proxy!"
+async fn server() {
+    let config = AppConfig::builder()
+        .add_env_prefixed("APP_PROXY")
+        .build()
+        .unwrap();
+
+    let app = Router::new().route("/proxy_server/*path", any(|| async { "Hello, Proxy!" }));
+
+    Application::new(&config)
+        .rest_router(app)
+        .serve()
+        .await
+        .unwrap();
 }
 
-async fn _proxy(req: Request<Body>) -> Result<Response, Error> {
-    trace!(?req);
-
-    if let Some(host_addr) = req.uri().authority().map(|auth| auth.to_string()) {
-        tokio::task::spawn(async move {
-            match hyper::upgrade::on(req).await {
-                Ok(upgraded) => {
-                    if let Err(e) = _tunnel(upgraded, host_addr).await {
-                        warn!("server io error: {}", e);
-                    };
-                }
-                Err(e) => warn!("upgrade error: {}", e),
-            }
-        });
-
-        Ok(Response::new(body::boxed(body::Empty::new())))
-    } else {
-        warn!("CONNECT host is not socket addr: {:?}", req.uri());
-        Ok((
-            StatusCode::BAD_REQUEST,
-            "CONNECT must be to a socket address",
-        )
-            .into_response())
-    }
-}
-
-async fn _tunnel(mut upgraded: Upgraded, addr: String) -> std::io::Result<()> {
-    let mut server = TcpStream::connect(addr).await?;
-
-    let (from_client, from_server) =
-        tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
-
-    debug!(
-        "client wrote {} bytes and received {} bytes",
-        from_client, from_server
-    );
-
-    Ok(())
-}
+/*
+will be handled by app:
+    curl http://0.0.0.0:8000/app/hello
+    curl http://0.0.0.0:8000/app/world
+will be redirected to another service:
+    curl http://0.0.0.0:8000/proxy_server/hello
+will not match any Router and won't be proxied:
+    curl http://0.0.0.0:8000/app/proxy_server/hello
+    curl http://0.0.0.0:8000/anything
+*/
