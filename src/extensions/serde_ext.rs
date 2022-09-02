88f97fc6fe76f79da8b8399c2e4ca377e79cc061
Aleksander <bratsinot@gmail.com>
2022-09-02 16:17:53 +0200

    Add DeserializeExt trait for overcoming config overlapping paths. (#46)
    * Add DeserializeExt trait for overcoming config overlapping paths.
    
    * move DeserializeExt to extentions/serde_ext
    
    Co-authored-by: nduchenk <adddyny@yandex.ru>

---
+++ src/extensions/serde_ext.rs
@@ -0,0 +1,26 @@
+use serde::de::Error;
+use serde::Deserialize;
+use serde_json::Value;
+
+// TODO: FIXME: https://github.com/serde-rs/serde/issues/2261
+/// Needed for overcoming overlapping path in config deserialization.
+pub trait DeserializeExt {
+    fn pointer_and_deserialize<'de, T, E>(&'de self, pointer: &'static str) -> Result<T, E>
+    where
+        T: Deserialize<'de>,
+        E: Error;
+}
+
+impl DeserializeExt for Value {
+    fn pointer_and_deserialize<'de, T, E>(&'de self, pointer: &'static str) -> Result<T, E>
+    where
+        T: Deserialize<'de>,
+        E: Error,
+    {
+        let raw_ret = self
+            .pointer(pointer)
+            .ok_or_else(|| E::missing_field(pointer))?;
+
+        T::deserialize(raw_ret).map_err(E::custom)
+    }
+}
