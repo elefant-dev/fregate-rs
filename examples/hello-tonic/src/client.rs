ae1e4af279959720a688b165d8369f2b1492abb8
nduchenk <adddyny@yandex.com>
2022-09-15 08:17:19 +0200

    refactor examples

--- examples/hello-tonic/src/client.rs
+++ examples/hello-tonic/src/client.rs
@@ -4,63 +4,70 @@ mod proto {
 
 use fregate::AppConfig;
 use opentelemetry::propagation::Injector;
-use proto::{hello_client::HelloClient, HelloRequest};
-use tonic::Request;
-use tracing::{info, info_span, Instrument};
+use proto::{hello_client::HelloClient, HelloRequest, HelloResponse};
+use tonic::transport::Channel;
+use tonic::{
+    metadata::{MetadataKey, MetadataValue},
+    Request, Response, Status,
+};
+use tracing::{info, Span};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
 
+// This code is taken from:
+// https://github.com/open-telemetry/opentelemetry-rust/blob/main/examples/grpc/src/client.rs
 pub struct MetadataMap<'a>(pub &'a mut tonic::metadata::MetadataMap);
 
 impl<'a> Injector for MetadataMap<'a> {
     /// Set a key and value in the MetadataMap.  Does nothing if the key or value are not valid inputs
     fn set(&mut self, key: &str, value: String) {
-        if let Ok(key) = tonic::metadata::MetadataKey::from_bytes(key.as_bytes()) {
-            if let Ok(val) = tonic::metadata::MetadataValue::from_str(&value) {
+        if let Ok(key) = MetadataKey::from_bytes(key.as_bytes()) {
+            if let Ok(val) = MetadataValue::try_from(value) {
                 self.0.insert(key, val);
             }
         }
     }
 }
 
-pub fn inject<T>(mut request: Request<T>) -> Request<T> {
+pub fn inject_context<T>(request: &mut Request<T>) {
     opentelemetry::global::get_text_map_propagator(|propagator| {
         propagator.inject_context(
-            &tracing::Span::current().context(),
+            &Span::current().context(),
             &mut MetadataMap(request.metadata_mut()),
         )
     });
-    request
 }
 
-#[tracing::instrument]
-async fn send_hello() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = tonic::transport::Endpoint::from_static("http://0.0.0.0:8000")
-        .connect()
-        .await
-        .unwrap();
-    let mut client = HelloClient::new(channel);
+#[tracing::instrument(name = "request")]
+async fn send_hello(
+    client: &mut HelloClient<Channel>,
+    mut request: Request<HelloRequest>,
+) -> Result<Response<HelloResponse>, Status> {
+    inject_context(&mut request);
 
-    let request = Request::new(HelloRequest {
-        name: "Tonic".into(),
-    });
+    info!("Outgoing Request: {:?}", request);
+    let response = client.say_hello(request).await;
+    info!("Incoming Response: {:?}", response);
 
-    let response = client
-        .say_hello(inject(request))
-        .instrument(info_span!("GreeterClient client request"))
-        .await?;
-
-    info!("RESPONSE={:?}", response);
-    Ok(())
+    response
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::env::set_var("OTEL_SERVICE_NAME", "CLIENT SIDE");
+    std::env::set_var("OTEL_SERVICE_NAME", "CLIENT");
     std::env::set_var("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT", "http://0.0.0.0:4317");
-
     let _config = AppConfig::default();
 
-    send_hello().await?;
+    let channel = tonic::transport::Endpoint::from_static("http://0.0.0.0:8000")
+        .connect()
+        .await
+        .unwrap();
+
+    let mut client = HelloClient::new(channel);
+    let request = Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    send_hello(&mut client, request).await?;
 
     Ok(())
 }
