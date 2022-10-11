05dccab78044dbfd4fe64389b340fd5c8006e9af
Ivan Tsyban <ivan.tsyban@genesys.com>
2022-10-11 22:07:12 +0200

    [Metrics] Add metrics functionality (#61)
        - add metrics config;
        - add request counter metric;
        - add labels and metrics according documentation
        - add response latency histogram metric;
        - add record metrics and add metrics route;

--- src/application/bootstrap.rs
+++ src/application/bootstrap.rs
@@ -51,6 +51,8 @@ where
         traces_endpoint.as_deref(),
     )?;
 
+    init_metrics()?;
+
     info!("Configuration: `{config:?}`.");
 
     Ok(config)
--- src/middleware/tracing.rs
+++ src/middleware/tracing.rs
@@ -1,7 +1,9 @@
-use hyper::{Request, Response};
+use axum::http::header::CONTENT_TYPE;
+use hyper::{header::HeaderMap, header::HeaderValue, Request, Response};
+use metrics::{histogram, increment_counter};
 use opentelemetry::{
     global::get_text_map_propagator,
-    trace::{TraceContextExt, TraceId},
+    trace::{SpanId, TraceContextExt, TraceId},
     Context,
 };
 use opentelemetry_http::HeaderExtractor;
@@ -67,29 +69,51 @@ pub struct BasicOnRequest;
 
 impl<B> OnRequest<B> for BasicOnRequest {
     fn on_request(&mut self, request: &Request<B>, span: &Span) {
-        let trace_id = get_span_trace_id(span);
+        let (trace_id, span_id) = get_trace_and_span_ids(span);
         let method = request.method();
         let uri = request.uri();
+        let protocol = get_protocol_type(request.headers());
 
-        info!("Incoming Request: method: [{method}], uri: {uri}, x-b3-traceid: {trace_id}");
+        info!("Incoming Request: method: [{method}], uri: {uri}, x-b3-traceid: {trace_id}, x-b3-spanid: {span_id}");
 
         span.record("method", &display(method));
         span.record("uri", &display(uri));
+
+        let labels = [
+            ("protocol", protocol.to_string()),
+            ("channel", "reqresp".to_string()),
+        ];
+
+        increment_counter!("traffic_count_total", &labels);
+        increment_counter!("traffic_sum_total");
     }
 }
 
 /// Logs message on response: "Outgoing Response: status code: {status}, latency: {latency}ms, x-b3-traceid: {trace_id}".
 #[derive(Clone, Copy, Debug)]
 pub struct BasicOnResponse;
-
 impl<B> OnResponse<B> for BasicOnResponse {
     fn on_response(self, response: &Response<B>, latency: Duration, span: &Span) {
-        let trace_id = get_span_trace_id(span);
+        let (trace_id, span_id) = get_trace_and_span_ids(span);
         let status = response.status();
-        let latency = latency.as_millis();
+        let latency_as_millis = latency.as_millis();
+        let latency_as_sec = latency.as_secs_f64();
+        let protocol = get_protocol_type(response.headers());
 
         info!(
-            "Outgoing Response: status code: {status}, latency: {latency}ms, x-b3-traceid: {trace_id}"
+            "Outgoing Response: status code: {status}, latency: {latency_as_millis}ms, x-b3-traceid: {trace_id}, x-b3-spanid: {span_id}"
+        );
+
+        let labels = [
+            ("protocol", protocol.to_string()),
+            ("channel", "reqresp".to_string()),
+            ("code", status.to_string()),
+        ];
+
+        histogram!(
+            "processing_duration_seconds_sum_total",
+            latency_as_sec,
+            &labels
         );
     }
 }
@@ -100,7 +124,7 @@ pub fn extract_context<B>(request: &Request<B>) -> Context {
 }
 
 /// Get TraceId from given [`Span`]
-pub fn get_span_trace_id(span: &Span) -> TraceId {
+pub fn get_trace_and_span_ids(span: &Span) -> (TraceId, SpanId) {
     let context = span.context();
     let span_ref = context.span();
     let span_context = span_ref.span_context();
@@ -108,8 +132,21 @@ pub fn get_span_trace_id(span: &Span) -> TraceId {
     // when logging trace_id, firstly set parent context for current span and then take from it trace_id
     // if context were invalid it will be generated, so we log correct trace_id
     if span_context.is_valid() {
-        span_context.trace_id()
+        (span_context.trace_id(), span_context.span_id())
     } else {
-        TraceId::INVALID
+        (TraceId::INVALID, SpanId::INVALID)
     }
+}
+
+fn get_protocol_type(headers: &HeaderMap<HeaderValue>) -> &str {
+    headers
+        .get(CONTENT_TYPE)
+        .map(|content_type| {
+            if content_type.as_bytes().starts_with(b"application/grpc") {
+                "grpc"
+            } else {
+                "http"
+            }
+        })
+        .unwrap_or("http")
 }
