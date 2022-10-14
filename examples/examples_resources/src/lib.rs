2a8f1719444cebe1e3f7495f7b869e7058faca43
Aleksandr Sinotov <bratsinot@gmail.com>
2022-10-14 12:29:40 +0200

    Remove certs.

--- examples/examples_resources/src/lib.rs
+++ examples/examples_resources/src/lib.rs
@@ -2,6 +2,3 @@
 pub mod proto;
 
 pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("./proto/description.bin");
-
-pub const TLS_CERTIFICATE: &[u8] = include_bytes!("../certs/server.crt");
-pub const TLS_PRIVATE_KEY: &[u8] = include_bytes!("../certs/server.key");
