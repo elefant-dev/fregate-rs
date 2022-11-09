eed6670bf7e9f35cf67a2d150a4633533cd5bad3
nduchenk <adddyny@yandex.com>
2022-11-09 18:40:02 +0100

    put reqwest into optional dep

--- src/extensions.rs
+++ src/extensions.rs
@@ -1,12 +1,14 @@
 //! Extensions traits for different crates
 mod axum_ext;
 mod axum_tonic;
+#[cfg(feature = "reqwest")]
 mod reqwest_ext;
 mod serde_ext;
 mod tonic_ext;
 
 pub use axum_ext::*;
 pub use axum_tonic::*;
+#[cfg(feature = "reqwest")]
 pub use reqwest_ext::*;
 pub use serde_ext::*;
 pub use tonic_ext::*;
