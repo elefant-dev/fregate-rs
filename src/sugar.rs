02db55841dba40dae6c9cdfaadc4cb5b1b2cd15f
Aleksander <bratsinot@gmail.com>
2022-11-17 13:05:14 +0200

    Add HashBuilder struct with sugar for easier hash calculation. (#127)

--- src/sugar.rs
+++ src/sugar.rs
@@ -1,3 +1,6 @@
 //! Collection of syntax-sugar-like functions
 
 pub mod grpc;
+mod hash_builder;
+
+pub use hash_builder::*;
---
+++ src/sugar/hash_builder.rs
@@ -0,0 +1,39 @@
+#[cfg(feature = "ahash")]
+use ahash::RandomState;
+#[cfg(not(feature = "ahash"))]
+use std::collections::hash_map::RandomState;
+use std::hash::{BuildHasher, Hash, Hasher};
+
+/// Alias for hash result type.
+pub type HashId = u64;
+
+/// Struct with sugar for easier hash calculation.
+#[derive(Default, Debug)]
+pub struct HashBuilder(RandomState);
+
+impl HashBuilder {
+    /// Creates new [`HashBuilder`]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Calculate hash for value.
+    /// # Example
+    /// ```rust
+    /// use fregate::sugar::HashBuilder;
+    ///
+    /// let hash_builder = HashBuilder::new();
+    ///
+    /// let str0_hash = hash_builder.calculate_hash("str0");
+    ///
+    /// let num0 = u64::MAX;
+    /// let num0_hash = hash_builder.calculate_hash(num0);
+    /// let num0_ref_hash = hash_builder.calculate_hash(&num0);
+    /// assert_eq!(num0_hash, num0_ref_hash);
+    /// ```
+    pub fn calculate_hash<T: Hash>(&self, value: T) -> HashId {
+        let mut s = self.0.build_hasher();
+        value.hash(&mut s);
+        s.finish()
+    }
+}
