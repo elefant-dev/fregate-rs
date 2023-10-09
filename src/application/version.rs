762969da55fb3e0617bb0c5a5fa9833af10ec3a4
nduchenk <adddyny@yandex.com>
2023-10-09 14:21:09 +0200

    typo

--- src/application/version.rs
+++ src/application/version.rs
@@ -2,12 +2,12 @@
 use crate::AppConfig;
 use axum::response::IntoResponse;
 
-/// Trait to implement custom health check which will be used to respond to health check requests
+/// Trait to implement custom /version response
 pub trait VersionExt<PrivateCfg>: Send + Sync + 'static + Clone {
     /// return type for health check
     type Response: IntoResponse;
 
-    /// returns [`Self::Response`] in response to configured endpoint. By default `/version`.
+    /// returns [`Self::Response`] in response to configured endpoint. Default: `/version`.
     /// For more information see [`crate::configuration::ManagementConfig`]
     fn get_version(&self, cfg: &AppConfig<PrivateCfg>) -> Self::Response;
 }
